4c3e1b137471c3db6e93af2b614c395b39338275
Tiago Ferreira Fernandes <darkfirev@gmail.com>
2014-12-02 21:18:19 -0200

    Fila com JMS, transformação de XLM para CSV usando EIP e content route base

--- src/main/java/camelinaction/ConstrutorRotas.java
+++ src/main/java/camelinaction/ConstrutorRotas.java
@@ -12,18 +12,27 @@ public class ConstrutorRotas extends RouteBuilder {
 	public void configure() throws Exception {
 
 		// Coloco a mensagem na fila correta
-		from("file:" + origem + "?noop=true")
-			.process(new LogProcessor())
-			.to("jms:incomingOrders");
+		from("file:" + origem + "?noop=true").process(new LogProcessor())
+				.choice().when(header("CamelFileName").endsWith(".xml"))
+				.to("jms:incomingOrdersXML")
+				.when(header("CamelFileName").endsWith(".csv"))
+				.to("jms:incomingOrdersCSV");
+
+		// Retiro a mensagem da fila
+		from("jms:incomingOrdersXML")
+		// transformo para o padrão desejado
+				.bean(new Transormer(), "transformContent")
+				// coloco a mensagem no destino final
+				.to("file:" + destino + "?fileName=${file:name.noext}.CSV")
+				// impeço o roteamento futuro desta mensagem
+				.end();
 		
-		// Retiro a mensagem da fila 
-		from("jms:incomingOrders")
-			//transformo para o padrão desejado 
-			.bean(new Transormer(), "transformContent")
-			//coloco a mensagem no destino final
-			.to("file:" + destino + "?fileName=${file:name.noext}.CSV")
-			//impeço o roteamento futuro desta mensagem
-			.end();
+		// Retiro a mensagem da fila
+		from("jms:incomingOrdersCSV")
+				// coloco a mensagem no destino final
+				.to("file:" + destino + "?fileName=${file:name.noext}.CSV")
+				// impeço o roteamento futuro desta mensagem
+				.end();
 	}
 
 }
